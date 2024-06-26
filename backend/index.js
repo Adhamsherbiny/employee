2c626dae31ac8adf2e359e86929c4900b18a6ee8
Adham Sherbiny <98004101+Adhamsherbiny@users.noreply.github.com>
2024-06-26 23:35:55 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import mysql from "mysql"
+import cors from "cors"
+const port = 5000
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const DBCON = mysql.createConnection({
+    host:"127.0.0.1",
+    user: "root",
+    password: "",
+    database: "employees"
+})
+
+app.get("/employee" , (req , res)=>{
+    DBCON.query("select * from employee_info" , (err , result)=>{
+        if (err) throw err
+        else{
+            res.send(result)
+        }
+    })
+    
+})
+
+app.listen(port , ()=>{
+    console.log("server is running")
+})
