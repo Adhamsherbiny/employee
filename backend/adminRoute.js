dcd032ded2378a024ba62c83c9935e568611db15
Adham Sherbiny <98004101+Adhamsherbiny@users.noreply.github.com>
2024-06-27 19:44:29 +0300

    updated V 0.0.4 'new route employee'

--- backend/adminRoute.js
+++ backend/adminRoute.js
@@ -1,9 +1,19 @@
 import express from "express"
+import DBCON from "./db.js"
 
 const AdminRoute = express.Router()
 
 AdminRoute.get("/admin" , (req , res)=>{
     res.send("Welcome in our Admin Panel")
+})
+
+AdminRoute.get("/employee" , (req , res)=>{
+    DBCON.query("SELECT * FROM employee_info" , (err , result)=>{
+        if(err) throw err
+        else{
+            res.status(200).json({result})
+        }
+    })
 })
 
 export default AdminRoute
