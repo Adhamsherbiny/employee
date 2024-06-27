32f88aea98da11b7cfdc7ebdd38ed4e0000caf1f
Adham Sherbiny <98004101+Adhamsherbiny@users.noreply.github.com>
2024-06-27 20:10:41 +0300

    updated V 0.0.5.0 'return connection with share database'

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,11 @@
 import mysql from "mysql"
-// const DBCON = mysql.createConnection({
-//     host:"blreoo8niqn2jjskadlv-mysql.services.clever-cloud.com",
-//     user: "utxnktank6pwkllr",
-//     password: "Ne8sl2S1zkpVceojXtmk",
-//     database: "blreoo8niqn2jjskadlv"
-// })
-
 const DBCON = mysql.createConnection({
-        host:"localhost",
-        user: "root",
-        password: "",
-        database: "employees"
-    })
-    
+    host:"blreoo8niqn2jjskadlv-mysql.services.clever-cloud.com",
+    user: "utxnktank6pwkllr",
+    password: "Ne8sl2S1zkpVceojXtmk",
+    database: "blreoo8niqn2jjskadlv"
+})
+
 
 DBCON.connect((error)=>{
     if (error) throw error
