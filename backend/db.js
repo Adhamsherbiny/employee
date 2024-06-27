eca84b04ebeab189f8cc48ac6424197f6eb1ea4b
Adham Sherbiny <98004101+Adhamsherbiny@users.noreply.github.com>
2024-06-27 19:53:52 +0300

    updated V 0.0.5.0 'new connection with other database'

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,18 @@
 import mysql from "mysql"
+// const DBCON = mysql.createConnection({
+//     host:"blreoo8niqn2jjskadlv-mysql.services.clever-cloud.com",
+//     user: "utxnktank6pwkllr",
+//     password: "Ne8sl2S1zkpVceojXtmk",
+//     database: "blreoo8niqn2jjskadlv"
+// })
+
 const DBCON = mysql.createConnection({
-    host:"blreoo8niqn2jjskadlv-mysql.services.clever-cloud.com",
-    user: "utxnktank6pwkllr",
-    password: "Ne8sl2S1zkpVceojXtmk",
-    database: "blreoo8niqn2jjskadlv"
-})
+        host:"127.0.0.1",
+        user: "root",
+        password: "",
+        database: "employees"
+    })
+    
 
 DBCON.connect((error)=>{
     if (error) throw error
